1ef20a67c1165ab5c5a487a6f34f8838c2c2c5c6
mangala-karunarathne <mangalaicc@gmail.com>
2023-03-16 17:04:55 +0530

    CHG |  Display Data in UI Table

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/categories/create',[CategoryController::class, 'create']);
+Route::get('/categories/index',[CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create',[CategoryController::class, 'create'])->name('categories.create');
 Route::post('/categories/store',[CategoryController::class, 'store'])->name('categories.store');
 Route::get('/elfinder', 'Barryvdh\Elfinder\ElfinderController@showIndex');
